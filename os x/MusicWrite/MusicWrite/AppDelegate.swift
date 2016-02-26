7d6908cc984ea30f0bde4d23c622747b576b0267
OwenMcGirr <o.a.mcgirr@gmail.com>
2016-02-26 12:15:25 +0000

    Add Xcode project

---
+++ os x/MusicWrite/MusicWrite/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MusicWrite
+//
+//  Created by Owen McGirr on 26/02/2016.
+//  Copyright © 2016 Owen McGirr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
