8f9ce6edb303bae93afbad54eff60e03c3ad3770
OwenMcGirr <o.a.mcgirr@gmail.com>
2016-02-27 17:54:08 +0000

    Finish arduino sketch

--- arduino/Piano/Piano.ino
+++ arduino/Piano/Piano.ino
@@ -8,7 +8,10 @@ void setup() {
 void loop() {
 }
 
-void playNote(int keys[], long duration, long delayBetween) {
+void playNote(String keysStr, long duration, long delayBetween) {
+  char keys[sizeof(keysStr)];
+  keysStr.toCharArray(keys, sizeof(keys));
+  
   for (int i = 0; i < sizeof(keys); i++) {
     Keyboard.press(keys[i]);
   }
@@ -18,5 +21,10 @@ void playNote(int keys[], long duration, long delayBetween) {
   Keyboard.releaseAll();
 
   delay(delayBetween);
+}
+
+void delaySeconds(int secs) {
+  long seconds = (long)secs * 1000;
+  delay(seconds);
 }
 
